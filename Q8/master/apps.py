bc8ea72eed5a90834f08a2a3dc07981fff6f64b1
dip456 <dipchaudhari808@gmail.com>
2024-05-23 14:41:15 +0530

    project setup complate

--- Q8/master/apps.py
+++ Q8/master/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProductstoreConfig(AppConfig):
+class MasterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'productstore'
+    name = 'master'
--- Q8/productstore/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from .models import ProductModel
-
-class ProductForm(forms.ModelForm):
-    class Meta : 
-        model = ProductModel
-        fields = ['product_name']
